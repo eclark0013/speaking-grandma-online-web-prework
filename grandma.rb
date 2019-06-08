001f82dd437ffe5f85fa4083217e6ec9ee46813d
Eric Clark <eclark0013@gmail.com>
2019-06-08 17:31:53 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,9 @@
 def speak_to_grandma(input)
   if input == "I LOVE YOU GRANDMA"
     puts "I LOVE YOU TOO PUMPKIN"
-  elsif input == input.upcase
+  elsif
     puts "NO, NOT SINCE 1938"
   else
-    "HUH?! SPEAK UP, SONNY!"
+    puts "HUH?! SPEAK UP, SONNY!"
 end
   
